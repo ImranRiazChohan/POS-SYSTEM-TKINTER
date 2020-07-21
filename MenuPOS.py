a8823b4e978e11b891703188f6bef613bfb3e13e
ImranRiazChohan <48764517+ImranRiazChohan@users.noreply.github.com>
2020-07-21 17:50:31 +0500

    Add files via upload

--- MenuPOS.py
+++ MenuPOS.py
@@ -135,7 +135,7 @@ class MenuFORM:
 
     #************Reciept************************
         self.Reciept_Frame = Frame(self.root, bg="white", relief=GROOVE)
-        self.Reciept_Frame.place(x=1200, y=100, height=550, width=300)
+        self.Reciept_Frame.place(x=1200, y=100, height=550, width=330)
         self.Reciept_label=Label(self.Reciept_Frame,text="Reciept",bg="black",fg="white",font=("times new roma",16,"bold"),relief=GROOVE).pack(fill=X)
         self.scrol_y=Scrollbar(self.Reciept_Frame,orient=VERTICAL)
         self.Reciept_text=Text(self.Reciept_Frame,yscrollcommand=self.scrol_y.set)
@@ -145,15 +145,15 @@ class MenuFORM:
     #****************ButtonForm********************************
         self.Button_Frame=Frame(self.root,bg="blue", relief=GROOVE)
         self.Button_Frame.place(x=0,y=660,width=1500,height=150)
-        self.Add_button = Button(self.Button_Frame,command=self.TotalAmmount, text="Add", font=("times new roman", 13, 'bold')).place(x=200,y=40,width=100,height=60)
-        self.Cancel_button = Button(self.Button_Frame, text="Cancel",font=("times new roman", 14, 'bold'), width=11, relief=SUNKEN).place(x=40,y=40,width=100,height=60)
+        self.Add_button = Button(self.Button_Frame,command=self.total_, text="Add", font=("times new roman", 13, 'bold')).place(x=200,y=40,width=100,height=60)
+        self.Cancel_button = Button(self.Button_Frame, command=self.Clean,text="Cancel",font=("times new roman", 14, 'bold'), width=11, relief=SUNKEN).place(x=40,y=40,width=100,height=60)
         self.Change_button = Button(self.Button_Frame, text="Change", font=("times new roman", 14, 'bold'), width=11,relief=SUNKEN).place(x=360,y=40,width=100,height=60)
-        self.Logout_button = Button(self.Button_Frame, text="LogOut", font=("times new roman", 14, 'bold'), width=11,relief=SUNKEN).place(x=520,y=40,width=100,height=60)
+        self.Logout_button = Button(self.Button_Frame, command=self.logout,text="LogOut", font=("times new roman", 14, 'bold'), width=11,relief=SUNKEN).place(x=520,y=40,width=100,height=60)
         self.Size_lable=Label(self.Button_Frame,text="Size",bg="black",fg="white",font=("times new roma",16,"bold"),relief=GROOVE).place(x=750,y=40,width=150,height=20)
         self.Size_Combox=ttk.Combobox(self.Button_Frame,width=20)
         self.Size_Combox["values"]=("small","larger","pan","regular","jumbo")
         self.Size_Combox.place(x=920,y=40,width=150,height=20)
-        self.Reciept_button=Button(self.Button_Frame,text="Reciept",font=("times new roman", 14, 'bold'), width=11, relief=SUNKEN).place(x=750,y=70,width=150,height=40)
+        self.Reciept_button=Button(self.Button_Frame,command=self.GenerateBill,text="Reciept",font=("times new roman", 14, 'bold'), width=11, relief=SUNKEN).place(x=750,y=70,width=150,height=40)
 
         self.casheirNameLabel=Label(self.Button_Frame,text="ChasierName",bg="black",fg="white",font=("times new roma",16,"bold"),relief=GROOVE).place(x=1080,y=40,width=160,height=20)
         self.casheirNameEntry = Label(self.Button_Frame,text="Name",font=("times new roman", 14, 'bold'), width=11, relief=SUNKEN).place(x=1250,y=40,width=150,height=20)
@@ -163,66 +163,158 @@ class MenuFORM:
         self.casheirEmailEntry = Label(self.Button_Frame,text="Email",font=("times new roman", 14, 'bold'), width=11, relief=SUNKEN).place(x=1250,y=100,width=150,height=20)
         self.total=IntVar()
         self.TotalLabel=Label(self.Button_Frame,textvariable=self.total,bg="white",fg="green",font=("times new roma",16,"bold"), width=11, relief=FLAT).place(x=920,y=70,width=150,height=40)
-    # def TotalAmmount(self):
-    #     self.deals={"deal1":{"price":349,"Detail":"abc"}}
-    #     self.total_ammount=(
-    #         (self.deal1.get()*self.deals["deal1"]["price"])+(self.deal2.get()*200)+(self.deal3.get()*200)+(self.deal4.get()*200)+(self.deal5.get()*200)+(self.deal6.get()*200))
-    #         # (self.Pizza1.get())+(self.Pizza2.get())+(self.Pizza3.get())+(self.Pizza4.get())+(self.Pizza5.get())+(self.Pizza6.get())+(self.Pizza7.get())+(self.Pizza8.get())+(self.Pizza9.get())+(self.Pizza10.get())+
-    #         # (self.Drink1.get())+(self.Drink2.get())+(self.Drink3.get())+(self.Drink4.get())+(self.Drink5.get())+(self.Drink6.get())+
-    #         # (self.Other1.get())+(self.Other2.get())+(self.Other3.get()))
-    #     self.total.set(str(self.total_ammount))
-class MainMenu(MenuFORM):
-    def __init__(self,root):
-        super().__init__(root)
-    def Menu(self):
-        super().Menu()
-    def TotalAmmount(self):
-        self.deals={"deal1":{"price":349,"Detail":"6Inch PIzza & Cold Drink(345ml)"},
-                    "deal2":{"price":949,"Detail":"RegularPizza & 2Drinks(345ml)"},
-                    "deal3":{"price":1349,"Detail":"12Inch Large Pizza & Drink(1.5ltr"},
-                    "deal4":{"price":2349,"Detail":"16Inch JumboPizza & Drink(1.5ltr)"},
-                    "deal5":{"price":599,"Detail":"LasagnePasta & Drink(345ml)"},
-                    "deal6":{"price":3449,"Detail":"2LargePizza & 5Drink(345ml)"}}
+    def RecieptGenerate_title(self):
+        self.Reciept_text.delete("1.0",END)
+        self.Reciept_text.insert(END,"\tSale Reciept")
+        self.Reciept_text.insert(END, "\nCashier Name:")
+        self.Reciept_text.insert(END, "\nToken No:")
+        self.Reciept_text.insert(END, "\nInvDate:")
+        self.Reciept_text.insert(END, "\nCostumer Name:Mr.Walking Costumer")
+        self.Reciept_text.insert(END, "\n*************************************")
+        self.Reciept_text.insert(END, "\nItemName   Price   Qty         Amount")
+        self.Reciept_text.insert(END, "\n*************************************")
+        pass
+    def Clean(self):
+        self.Drink6.set(0)
+        self.Drink5.set(0)
+        self.Drink2.set(0)
+        self.Drink3.set(0)
+        self.Drink1.set(0)
+        self.Drink4.set(0)
 
-        self.Drinks={"Drink1":{"Price":100,"Name":"Sevenup"},
-                     "Drink1":{"Price":100,"Name":"Pepsi"},
-                     "Drink1":{"Price":100,"Name":"Dew"},
-                     "Drink1":{"Price":100,"Name":"Marinda"},
-                     "Drink1":{"Price":100,"Name":"Coke"},
-                     "Drink1":{"Price":100,"Name":"Sprite"}}
+        self.Other1.set(0)
+        self.Other3.set(0)
+        self.Other2.set(0)
+
+        self.deal1.set(0)
+        self.deal2.set(0)
+        self.deal3.set(0)
+        self.deal4.set(0)
+        self.deal5.set(0)
+        self.deal6.set(0)
+
+        self.Pizza1.set(0)
+        self.Pizza2.set(0)
+        self.Pizza3.set(0)
+        self.Pizza4.set(0)
+        self.Pizza5.set(0)
+        self.Pizza6.set(0)
+        self.Pizza7.set(0)
+        self.Pizza8.set(0)
+        self.Pizza9.set(0)
+        self.Pizza10.set(0)
+        self.total.set(0)
+    def Update(self):
+        pass
+    def logout(self):
+        self.root.quit()
+    def GenerateBill(self):
+        self.RecieptGenerate_title()
+
+        if self.deal1.get()!=0:
+            self.Reciept_text.insert(END,f"\nDeal1\t   {self.deals['deal1']['price'] }\t    {self.deal1.get()}\t\t{self.d1_p}")
+        if self.deal2.get()!=0:
+            self.Reciept_text.insert(END,f"\nDeal2\t   {self.deals['deal2']['price'] }\t    {self.deal2.get()}\t\t{self.d2_p}")
+        if self.deal3.get()!=0:
+            self.Reciept_text.insert(END,f"\nDeal3\t   {self.deals['deal3']['price'] }\t    {self.deal3.get()}\t\t{self.d3_p}")
+        if self.deal4.get()!=0:
+            self.Reciept_text.insert(END,f"\nDeal4\t   {self.deals['deal4']['price'] }\t    {self.deal4.get()}\t\t{self.d4_p}")
+        if self.deal5.get()!=0:
+            self.Reciept_text.insert(END,f"\nDeal5\t   {self.deals['deal5']['price'] }\t    {self.deal5.get()}\t\t{self.d5_p}")
+        if self.deal6.get()!=0:
+            self.Reciept_text.insert(END,f"\nDeal6\t   {self.deals['deal6']['price'] }\t    {self.deal6.get()}\t\t{self.d6_p}")
+
+        # if self.Drink1.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Drinks['Drink1']['Name']}\t   {self.Drinks['Drink1']['price']}\t    {self.Drink1.get()}\t\t{self.dr1_p}")
+        # if self.Drink2.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Drinks['Drink2']['Name']}\t   {self.Drinks['Drink2']['price']}\t    {self.Drink2.get()}\t\t{self.dr2_p}")
+        # if self.Drink3.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Drinks['Drink3']['Name']}\t   {self.Drinks['Drink3']['price']}\t    {self.Drink3.get()}\t\t{self.dr3_p}")
+        # if self.Drink4.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Drinks['Drink4']['Name']}\t   {self.Drinks['Drink4']['price']}\t    {self.Drink4.get()}\t\t{self.dr4_p}")
+        # if self.Drink5.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Drinks['Drink5']['Name']}\t   {self.Drinks['Drink5']['price']}\t    {self.Drink5.get()}\t\t{self.dr5_p}")
+        # if self.Drink6.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Drinks['Drink6']['Name']}\t   {self.Drinks['Drink6']['price']}\t    {self.Drink6.get()}\t\t{self.dr6_p}")
+
+        if self.Other1.get()!=0:
+            self.Reciept_text.insert(END,f"\n{self.Others['Other1']['Name']}\t   {self.Others['Other1']['Price']}\t    {self.Other1.get()}\t\t{self.o1_p}")
+        if self.Other2.get()!=0:
+            self.Reciept_text.insert(END,f"\nD{self.Others['Other2']['Name']}\t   {self.Others['Other2']['Price']}\t   {self.Other2.get()}\t\t{self.o2_p}")
+        if self.Other3.get()!=0:
+            self.Reciept_text.insert(END,f"\n{self.Others['Other3']['Name']}\t   {self.Others['Other3']['Price']}\t    {self.Other3.get()}\t\t{self.o3_p}")
+
+        # if self.Pizza1.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Pizzas['pizza1']['name']} \t   {self.Pizzas['pizza1']['price'] }\t    {self.Pizza1.get()}\t\t{self.p1_p}")
+        # if self.Pizza2.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Pizzas['pizza1']['name']}\t   {self.Pizzas['pizza2']['price'] }\t    {self.Pizza2.get()}\t\t{self.p2_p}")
+        # if self.Pizza3.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Pizzas['pizza1']['name']}\t   {self.Pizzas['pizza3']['price'] }\t    {self.Pizza3.get()}\t\t{self.p3_p}")
+        # if self.Pizza4.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Pizzas['pizza1']['name']}\t   {self.Pizzas['pizza4']['price'] }\t    {self.Pizza4.get()}\t\t{self.p4_p}")
+        # if self.Pizza5.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Pizzas['pizza1']['name']}\t   {self.Pizzas['pizza5']['price'] }\t    {self.Pizza5.get()}\t\t{self.p5_p}")
+        # if self.Pizza6.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Pizzas['pizza1']['name']}\t   {self.Pizzas['pizza6']['price'] }\t    {self.Pizza6.get()}\t\t{self.p6_p}")
+        # if self.Pizza7.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Pizzas['pizza1']['name']}\t   {self.Pizzas['pizza7']['price'] }\t    {self.Pizza7.get()}\t\t{self.p7_p}")
+        # if self.Pizza8.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Pizzas['pizza1']['name']}\t   {self.Pizzas['pizza8']['price'] }\t    {self.Pizza8.get()}\t\t{self.p8_p}")
+        # if self.Pizza9.get()!=0:
+        #     self.Reciept_text.insert(END,f"\n{self.Pizzas['pizza1']['name']}\t   {self.Pizzas['pizza9']['price'] }\t    {self.Pizza9.get()}\t\t{self.p9_p}")
+        # if self.Pizza10.get()!=0:
+        #     self.Reciept_text.insert(END,f"\ns{self.Pizzas['pizza1']['name']}\t   {self.Pizzas['pizza10']['price'] }\t  {self.Pizza10.get()}\t\t{self.p10_p}")
+
+    def total_(self):
+        self.deals = {"deal1": {"price": 349, "Detail": "6Inch PIzza & Cold Drink(345ml)"},
+                        "deal2":{"price":949,"Detail":"RegularPizza & 2Drinks(345ml)"},
+                        "deal3":{"price":1349,"Detail":"12Inch Large Pizza & Drink(1.5ltr"},
+                        "deal4":{"price":2349,"Detail":"16Inch JumboPizza & Drink(1.5ltr)"},
+                        "deal5":{"price":599,"Detail":"LasagnePasta & Drink(345ml)"},
+                        "deal6":{"price":3449,"Detail":"2LargePizza & 5Drink(345ml)"}}
+
+
+        # self.Drinks={"Drink1":{"Price":100,"Name":"Sevenup"},
+        #                 "Drink2":{"Price":100,"Name":"Pepsi"},
+        #                 "Drink3":{"Price":100,"Name":"Dew"},
+        #                 "Drink4":{"Price":100,"Name":"Marinda"},
+        #                 "Drink5":{"Price":100,"Name":"Coke"},
+        #                 "Drink6":{"Price":100,"Name":"Sprite"}}
 
         self.Others={"Other1":{"Price":300,"Name":"Salad"},
-                     "Other1":{"Price":500,"Name":"LasagnaPast"},
-                     "Other1":{"Price":700,"Name":"ChickenTikka"}}
+                         "Other2":{"Price":500,"Name":"LasagnaPast"},
+                         "Other3":{"Price":700,"Name":"ChickenTikka"}}
 
-        self.Size={"large":{"Price":1500},
-                     "regular":{"Price":500},
-                     "jumbo":{"Price":1800},
-                     "345ml": {"Price": 50},
-                     "1.5ml": {"Price": 90},
-                     "2.15ml": {"Price": 160}}
-        self.total_ammount=(
-                ((self.deal1.get()*self.deals["deal1"]["price"])+
-            (self.deal2.get()*self.deals["deal2"]["price"])+
-            (self.deal3.get()*self.deals["deal3"]["price"])+
-            (self.deal4.get()*self.deals["deal4"]["price"])+
-            (self.deal5.get()*self.deals["deal5"]["price"])+
-            (self.deal6.get()*self.deals["deal6"]["price"]))+
-            # (self.Pizza1.get()*self.Size)+(self.Pizza2.get())+(self.Pizza3.get())+(self.Pizza4.get())+(self.Pizza5.get())+(self.Pizza6.get())+(self.Pizza7.get())+(self.Pizza8.get())+(self.Pizza9.get())+(self.Pizza10.get())+
-                ((self.Drink1.get()*self.Drinks["Drink1"]["Price"])+(self.Drink2.get()*self.Drinks["Drink2"]["Price"])+
-            (self.Drink3.get()*self.Drinks["Drink3"]["Price"])+(self.Drink4.get()*self.Drinks["Drink4"]["Price"])+
-            (self.Drink5.get()*self.Drinks["Drink5"]["Price"])+(self.Drink6.get()*self.Drinks["Drink6"]["Price"]))+
-            (self.Other1.get()*self.Others["Other1"]["Price"])+(self.Other2.get()*self.Others["Other2"]["Price"])+
-                (self.Other3.get()*self.Others["Other3"]["Price"]))
-        self.total.set(str(self.total_ammount))
+        # self.Size={"large":{"Price":1500},
+        #                  "regular":{"Price":500},
+        #                  "jumbo":{"Price":1800},
+        #                  "345ml": {"Price": 50},
+        #                  "1.5ml": {"Price": 90},
+        #                  "2.15ml": {"Price": 160}}
 
+        self.d1_p=self.deal1.get()*self.deals["deal1"]["price"]
+        self.d2_p=self.deal2.get()*self.deals["deal2"]["price"]
+        self.d3_p=self.deal3.get()*self.deals["deal3"]["price"]
+        self.d4_p=self.deal4.get()*self.deals["deal4"]["price"]
+        self.d5_p=self.deal5.get()*self.deals["deal5"]["price"]
+        self.d6_p=self.deal6.get()*self.deals["deal6"]["price"]
+
+        self.o1_p=self.Other1.get()*self.Others["Other1"]["Price"]
+        self.o2_p = self.Other2.get() * self.Others["Other2"]["Price"]
+        self.o3_p = self.Other3.get() * self.Others["Other3"]["Price"]
+
+        self.total_others=float(self.o1_p+self.o2_p+self.o3_p)
+        self.total_deals=float(self.d1_p+self.d2_p+self.d3_p+self.d4_p+self.d5_p+self.d6_p)
+
+        self.grand_total=float(self.total_deals+self.total_others)
+        self.total.set("Rs: "+str(self.grand_total))
 
 
 
 if __name__=="__main__":
     root=tk.Tk()
-    # menu=Menu(root)
-    # menu.Menu()
-    m1=MainMenu(root)
-    m1.Menu()
+    menu=MenuFORM(root)
+    menu.Menu()
+    # m1=MainMenu(root)
+    # m1.Menu()
     root.mainloop()
